1d15b3109f7c0aabf852723ac77e62b1766fc51b
Olabode Adebayo <webframes@gmail.com>
2020-02-14 20:03:08 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <router-view/>
+
+    <footer class="pb-4 hr">
+      <hr class="mt-5 mb-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <img src="images/logo.png" width="41" height="36" class="float-left mt-sm-1 mr-3" alt="">
+            <ul class="list-unstyled">
+              <li>Sources on <a href="https://inksnation.io" target="_blank">GitHub</a></li>
+            
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style>
+  input:invalid {
+    box-shadow: none;
+  }
+
+  code {
+    word-break: break-all;
+    word-wrap: break-word;
+  }
+</style>
